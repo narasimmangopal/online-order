93eea648d385c9741aa5b99ecc6cb1a986a299ca
Narasimman <narasimman97@gmail.com>
2020-09-30 16:09:02 +0530

    oline order

---
+++ online order.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter import messagebox
+l=Tk()
+l.geometry("500x400")
+l.title("checking availability")
+l.config(bg="black")
+Label(text="Product").grid(row=0,column=0)
+Product=Entry()
+Product.grid(row=0,column=1)
+Label(text="count").grid(row=1,column=0)
+count=Entry()
+count.grid(row=1,column=1)
+def check():
+    s=Product.get()
+    b =count.get()
+    f=open("product details.txt","r")
+    for i in f:
+        if(i.split(" ")[0]==s and b<=i.split(" ")[1]):
+            print(s)
+            messagebox.showinfo("check","your product is available for purchase")
+            break
+    else:
+        messagebox.showinfo("check","Out of Stock")
+    f.close()
+Button(text="check",command=check).grid(row=2,column=0,columnspan=2)
+l.mainloop()
